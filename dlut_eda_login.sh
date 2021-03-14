eb2c2c17c51b21fedc73adb9dedaedc99d96cc3b
M3GABOY <65479432+M3GABOY@users.noreply.github.com>
2021-03-14 22:24:34 +0800

    Update dlut_eda_login.sh

--- dlut_eda_login.sh
+++ dlut_eda_login.sh
@@ -12,7 +12,7 @@ done
 auth()
 {
     echo "Trying  to login as username: ${username}"
-    curl -s --header "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9" --compressed --header "Accept-Language: en-US,en;q=0.9" --header "Cache-Control: max-age=0" --header "Connection: keep-alive" --header "Origin: http://172.20.20.1:801" --header "Referer: http://172.20.20.1:801/srun_portal_pc.php?ac_id=3&" --header "Upgrade-Insecure-Requests: 1" --user-agent "Mozilla/5.0 (Windows; U; Windows NT 4.0) AppleWebKit/533.43.4 (KHTML, like Gecko) Version/4.0.5 Safari/533.43.4" --data-binary "action=login&ac_id=3&user_ip=&nas_ip=&user_mac=&url=&username=${username}&password=${password}" "http://172.20.20.1:801/srun_portal_pc.php?ac_id=3&" > /dev/null
+    curl -s --header --connect-timeout 4 "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9" --compressed --header "Accept-Language: en-US,en;q=0.9" --header "Cache-Control: max-age=0" --header "Connection: keep-alive" --header "Origin: http://172.20.20.1:801" --header "Referer: http://172.20.20.1:801/srun_portal_pc.php?ac_id=3&" --header "Upgrade-Insecure-Requests: 1" --user-agent "Mozilla/5.0 (Windows; U; Windows NT 4.0) AppleWebKit/533.43.4 (KHTML, like Gecko) Version/4.0.5 Safari/533.43.4" --data-binary "action=login&ac_id=3&user_ip=&nas_ip=&user_mac=&url=&username=${username}&password=${password}" "http://172.20.20.1:801/srun_portal_pc.php?ac_id=3&" > /dev/null
 
     echo "Checking Internet connection..."
     stat_code=0
