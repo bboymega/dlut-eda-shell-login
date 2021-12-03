684b2baf4ce120a1557793040b1bc8be41eac31b
Bboy Mega <i@bboy.live>
2021-12-03 11:08:48 +0800

    Update dlut_eda_login.sh

--- dlut_eda_login.sh
+++ dlut_eda_login.sh
@@ -24,7 +24,7 @@ auth()
 
 unauth()
 {
-    stat_code=$(curl -s --connect-timeout 5 --compressed --header "Accept-Language: en-US,en;q=0.9" --header "Connection: keep-alive" --header "Origin: http://172.20.20.1:801" --header "Referer: http://172.20.20.1:801/srun_portal_pc.php?ac_id=3&" --user-agent "Mozilla/5.0 (Windows; U; Windows NT 4.0) AppleWebKit/533.43.4 (KHTML, like Gecko) Version/4.0.5 Safari/533.43.4" --header "X-Requested-With: XMLHttpRequest" --data-binary "action=logout&username=&password=&ajax=1" "http://172.20.20.1:801/include/auth_action.php")
+    stat_code=$(curl -s --connect-timeout 5 --header "Accept-Language: en-US,en;q=0.9" --header "Connection: keep-alive" --header "Origin: http://172.20.20.1:801" --header "Referer: http://172.20.20.1:801/srun_portal_pc.php?ac_id=3&" --user-agent "Mozilla/5.0 (Windows; U; Windows NT 4.0) AppleWebKit/533.43.4 (KHTML, like Gecko) Version/4.0.5 Safari/533.43.4" --header "X-Requested-With: XMLHttpRequest" --data-binary "action=logout&username=&password=&ajax=1" "http://172.20.20.1:801/include/auth_action.php")
     if [ "${stat_code}" ==  "网络已断开" ]; then
     echo "Successfully logged out."
   else if [ "${stat_code}" ==  "您似乎未曾连接到网络..." ]; then
