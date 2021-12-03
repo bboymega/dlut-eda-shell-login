ee0a66e86bfbb2c02378ccdc0eb30dc9a35cdfdf
Bboy Mega <i@bboy.live>
2021-12-03 11:06:15 +0800

    Update check_internet.sh

--- check_internet.sh
+++ check_internet.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 case "$(curl -s --max-time 2 -I http://connectivitycheck.gstatic.com/generate_204 | sed 's/^[^ ]* *\([0-9]\).*/\1/; 1q')" in
 [23]) exit ;;
-5) $(dirname "$0")/dlut_eda_login.sh -u "" -p ""
-*) $(dirname "$0")/dlut_eda_login.sh -u "" -p ""
+5) $(dirname "$0")/dlut_eda_login.sh -u "" -p "" #fill the -u with username and -p with password
+*) $(dirname "$0")/dlut_eda_login.sh -u "" -p "" #fill the -u with username and -p with password
 esac
